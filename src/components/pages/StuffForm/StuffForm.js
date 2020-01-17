a27f91e29cf8e94a65f6f10701b11fb9cfb8794e
John Thielman <john.thielman@gmail.com>
2020-01-16 19:36:26 -0600

    form shows correct heading depending on link clicked to get there

--- src/components/pages/StuffForm/StuffForm.js
+++ src/components/pages/StuffForm/StuffForm.js
@@ -3,18 +3,18 @@ import React from 'react';
 import './StuffForm.scss';
 
 class StuffForm extends React.Component {
-  componentDidMount() {
+  /* componentDidMount() {
     if (this.props.path === '/stuff/new') {
       console.log('new stuff page');
     } else {
       console.log('edit stuff page');
     }
-  }
+  } */
 
   render() {
     return (
       <div className='StuffForm'>
-        <h1>Form</h1>
+        { this.props.path === '/stuff/new' ? <h1>New Stuff</h1> : <h1>Edit</h1> }
       </div>
     );
   }
